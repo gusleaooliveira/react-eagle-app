0d21498e170dee3b4eee768183f574322f500493
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2021-05-05 15:34:39 -0300

    menu e cabecalho concluídos

--- src/components/Cabecalho.js
+++ src/components/Cabecalho.js
@@ -1,7 +1,13 @@
+import { faDove } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function Cabecalho(props){
-    return <div></div>
+    return <div className="padding-10 raisin-black row sticky-top">
+                <IconeUsuario icone={props.icone} />
+                <h1 className="text-xlarge col-10 margin-left-5"> Nome do usuário</h1>
+                <FontAwesomeIcon icon={faDove} className="text-xlarge col-1" />
+            </div>
 }
 
-export default Cabecalho;
+export default Cabecalho 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { faAddressBook, faComment, faComments, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link} from 'react-router-dom'
 
 function Menu(props){
-    return <div></div>
+    return  <nav className="bar bar-mobile raisin-black padding-5">
+                <Link to="/contatos" className="btn btn-block eggplant">
+                    <span className="hide-on-mobile">Contatos</span>
+                    <FontAwesomeIcon icon={faAddressBook} />
+                </Link>
+                <Link to="/mensagens" className="btn btn-block eggplant">
+                    <span className="hide-on-mobile">Mensagens</span>
+                    <FontAwesomeIcon icon={faComments} />
+                </Link>
+                <Link to="/logout" className="btn btn-block eggplant">
+                    <span className="hide-on-mobile">Logout</span>
+                    <FontAwesomeIcon icon={faSignOutAlt} />
+                </Link>
+            </nav>
 }
 
 export default Menu;
