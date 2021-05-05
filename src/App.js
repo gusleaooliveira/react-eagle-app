0134e4e3009cb92ec8162cd97220d891b3f2f453
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2021-05-05 01:02:34 -0300

    icone  criado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './css/estilo.css';
 import './css/hcw.min.css';
-import usuario from './usuario.svg' 
-
+import IconeUsuario from './components/IconeUsuario';
 
 function App() {
   return (
     <React.Fragment>
-        <img src={usuario} className="icon-large" />
+        <IconeUsuario />
     </React.Fragment>
   );
 }
---
+++ src/components/IconeUsuario.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import usuario  from '../../src/usuario.svg'
+
+function IconeUsuario(props){
+    return <img src={usuario} className="icon-large border-circle" />
+}
+
+export default IconeUsuario;
