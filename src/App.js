53a532930c4701b9c78692fe997e73909383f1ef
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2021-05-05 03:11:23 -0300

    login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './css/estilo.css';
 import './css/hcw.min.css';
 import IconeUsuario from './components/IconeUsuario';
+import Login from './components/usuario/Login'
 
 function App() {
   return (
     <React.Fragment>
-        <IconeUsuario />
+        <Login />
     </React.Fragment>
   );
 }
--- src/components/usuario/Login.js
+++ src/components/usuario/Login.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faTimes } from '@fortawesome/free-solid-svg-icons';
+import IconeUsuario from '../IconeUsuario';
 
 function Login(props){
-    return <div></div>
+    return  <section className="padding-10 margin-10 card light-gray display-container">
+                
+                <div className="text-center  padding-16">
+                    <IconeUsuario />
+                </div>
+                
+                <label htmlFor="email">Email:</label>
+                <input type="email" placeholder="Digite seu email" id="email" className="white input border-bottom-eggplant-focus" />
+                
+                <label htmlFor="senha">Senha:</label>
+                <input type="password" placeholder="Digite sua senha" id="senha" className="white input border-bottom-eggplant-focus" />
+                
+                <button className="btn btn-block eggplant margin-top-5">
+                    <span>Logar </span>
+                    <FontAwesomeIcon icon={faUser} />
+                </button>
+                
+                <button className="btn eggplant display-top-right margin-10">
+                    <FontAwesomeIcon icon={faTimes} />
+                </button>
+            </section>;
 }
 
 export default Login;
